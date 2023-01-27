d095d974348d444a0506a3901517c70c7dfce38f
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2023-01-27 22:30:28 +0500

    Исправление бага

--- TelegramBot/TelegramHandler.cs
+++ TelegramBot/TelegramHandler.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using Archivator;
 using Telegram.Bot;
-using Telegram.Bot.Requests;
+using Archivator;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.InputFiles;
 
@@ -17,112 +12,101 @@ namespace TelegramBot
 {
     public class TelegramHandler
     {
-        private string token;
-        private TelegramBotClient botClient;
         private IArchivator archivator;
+        private string token;
+        private TelegramBotClient client;
         private bool areFilesUploaded;
+
         public TelegramHandler(IArchivator archivator, string token)
         {
             this.archivator = archivator;
             this.token = token;
-            this.botClient = new TelegramBotClient(token);
+            client = new TelegramBotClient(token);
         }
-
         public void Execute()
         {
-            botClient.StartReceiving(OnUpdate, OnError);
+            client.StartReceiving(UpdateHandler, ExceptionHandler);
         }
 
-        private async Task OnError(ITelegramBotClient client, Exception exception, CancellationToken token)
+        private async Task ExceptionHandler(ITelegramBotClient client, Exception exception,
+            CancellationToken token)
         {
-             Console.WriteLine(exception.StackTrace+"\n"+exception);
-            return;
+            throw new NotImplementedException();
         }
 
-        private async Task OnUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+        private async Task UpdateHandler(ITelegramBotClient client, Update update,
+            CancellationToken token)
         {
             var message = update.Message;
-            
+            var id = message.Chat.Id.ToString();
             if (areFilesUploaded)
             {
-                if (message.Text.EndsWith(".zip"))
-                    await SendArchive(message.Chat.Id.ToString(), 
-                        $"{message.Chat.Id}\\{message.Text}", message.Text);
-                await botClient.SendTextMessageAsync(message.Chat.Id,
-                    "Архив готовится!");
+                var fileName = message.Text.EndsWith(".zip") ? message.Text :
+                    $"{message.Text}.zip";
+                var path = $"{id}\\{fileName}";
+                SendArchive(id, path, fileName);
+                
                 return;
             }
-            if (message.Text == "/start")
-            {
-                await botClient.SendTextMessageAsync(message.Chat.Id,
-                    "Кидай мне файлы, как закончишь, пиши /end");
-                return;
-            }
-
+            else if (message.Text == "/start")
+               await client.SendTextMessageAsync(id,
+                    "Закидывай в меня файлы, как закончишь, пиши /end");
             else if (message.Text == "/end")
             {
                 areFilesUploaded = true;
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Введите имя архива:");
+                await client.SendTextMessageAsync(id,
+                   "Введите имя архива");
             }
-            else GetFile(update.Message);
-        }
-
-        private async Task SendArchive(string id, string path, string outFileName)
-        {
-            foreach (var file in Directory.EnumerateFiles(id.ToString()))
-                archivator.AddToArchive(file);
-            archivator.Compress(path);
-            using Stream stream = System.IO.File.OpenRead(path);
-            var document = new InputOnlineFile(stream, outFileName);
-           var task = botClient.SendDocumentAsync(id, document);
-            await task;
-            if (task.IsCompletedSuccessfully)
-            {
-                stream.Close();
-                Directory.Delete(id, true);
-                areFilesUploaded = false;
-               await botClient.SendTextMessageAsync(id,
-                    "Можно делать следующий архив, как закинешь файлы, пиши /end");
-
-            }
-            return;
-        }
-
-        private void GetFile(Message message)
-        {
-            if (message.Document != null)
-            {
-
-                DownloadFile(message.Document.FileId, message.Document.FileName,
-                    message.Chat.Id.ToString());
-            }
+            else if (message.Document != null)
+                DownloadFile(message.Document.FileId, message.Document.FileName, id);
+            else if (message.Audio != null)
+                DownloadFile(message.Audio.FileId, message.Audio.FileName, id);
             else if (message.Photo != null)
+                DownloadPhoto(message.Photo, id);
+        }
+
+        private void DownloadPhoto(PhotoSize[] photo, string id)
+        {
+            var fileId = photo[photo.Length - 1].FileId;
+            DownloadFile(fileId, fileId+".jpg", id);
+        }
+
+        private void DeleteTempFiles(string id)
+        {
+            Directory.Delete(id, true);
+        }
+
+        private async void DownloadFile(string fileId, string fileName, string id)
+        {
+            if (!Directory.Exists(id))
+                Directory.CreateDirectory(id);
+            using (var stream = new FileStream($"{id}\\{fileName}", FileMode.OpenOrCreate))
             {
-                var file = message.Photo[message.Photo.Length - 1];
-                var name = file.FileUniqueId.ToString() + ".jpg";
-                var chatId = message.Chat.Id.ToString();
-                DownloadFile(file.FileId, name, chatId);
-            }
-            else if (message.Audio!=null)
-            {
-                DownloadFile(message.Audio.FileId, message.Audio.FileName,
-                    message.Chat.Id.ToString());
-            }
-            else if (message.Video!=null)
-            {
-                DownloadFile(message.Video.FileId, message.Video.FileName, message.Chat.Id.ToString());
+                var info = await client.GetFileAsync(fileId);
+                var path = info.FilePath;
+                await client.DownloadFileAsync(path, stream);
             }
         }
 
-        private void DownloadFile(string fileId, string fileName, string directory)
+        private async Task SendArchive(string id, string path, string fileName)
         {
-            if (!Directory.Exists(directory))
-                Directory.CreateDirectory(directory);
-         using (var fileStream = new FileStream($"{directory}\\{fileName}", FileMode.OpenOrCreate))
+            foreach (var file in Directory.EnumerateFiles(id))
             {
-                var info = botClient.GetInfoAndDownloadFileAsync(fileId, fileStream);
-                Console.WriteLine(info.Result.FileSize);
+                archivator.AddToArchive(file);
             }
+
+            archivator.Compress(path);
+            using (var stream = System.IO.File.OpenRead(path))
+            {
+                var document = new InputOnlineFile(stream, fileName);
+                await client.SendDocumentAsync(id, document);
+            }
+            var sending = client.SendTextMessageAsync(id,
+                "Можешь создавать новый архив по окончании пиши /end");
+            await sending;
+            if (sending.IsCompletedSuccessfully)
+                DeleteTempFiles(id);
+            areFilesUploaded = false;
         }
     }
 }
