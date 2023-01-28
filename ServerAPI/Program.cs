707737f99d11aa41676c9d6f3d42e4da86f13fea
evgenyShinkarev2000 <evgeny.shinkarev@urfu.me>
2023-01-28 11:14:28 +0500

    Удалил контроллер погоды

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -19,11 +19,11 @@ namespace ServerAPI
 
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
